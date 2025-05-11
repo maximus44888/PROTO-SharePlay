619e48f36d98f8c33d8527f548e6a287d9b18009
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2025-05-11 20:57:58 +0200

    some more client/server testing/refactoring

--- Client/src/main/kotlin/Client.kt
+++ Client/src/main/kotlin/Client.kt
@@ -1,42 +1,56 @@
 package tfg.proto.shareplay
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 import java.io.DataInputStream
 import java.io.DataOutputStream
 import java.net.InetAddress
 import java.net.Socket
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-fun main() {
-    Socket(InetAddress.getByName("localhost"), 1235).use { socket ->
+const val mediaPath =
+    """C:\Users\jmaxi\Mis ficheros\Anime\86 S01+SP 1080p Dual Audio BDRip 10 bits DD+ x265-EMBER\86 S01P01+SP 1080p Dual Audio BDRip 10 bits DD+ x265-EMBER\S01E01-Undertaker [2F703024].mkv"""
+
+@OptIn(ExperimentalCoroutinesApi::class)
+suspend fun main() {
+    Socket(InetAddress.getByName("localhost"), 1234).use { socket ->
         println("Connected to server at ${socket.inetAddress.hostAddress}:${socket.localPort}")
 
         DataInputStream(socket.inputStream).use { objectInputStream ->
             DataOutputStream(socket.outputStream).use { objectOutputStream ->
-                val roomId = readln().toInt()
-                objectOutputStream.writeInt(roomId)
-                println("Asked to join room $roomId")
+                print("Enter room name: ")
+                objectOutputStream.writeUTF(readln())
+                coroutineScope {
+                    val requests: ReceiveChannel<IPC.Request> = produce {
+                        launch {
+                            send(IPC.Request.LoadFile(mediaPath))
+                            send(IPC.Request.SetProperty(IPC.Property.VOLUME, 0))
+                            send(IPC.Request.SetProperty(IPC.Property.PAUSE, true))
 
-                runBlocking {
-                    launch {
-                        while (true) {
-                            val message = withContext(Dispatchers.IO) {
-                                objectInputStream.readUTF()
+                            val randomPlaybackTime = (0..666).random()
+                            println("Random playback time: $randomPlaybackTime")
+
+                            while (true) {
+                                withContext(Dispatchers.IO) { readln() }
+                                objectOutputStream.writeInt(randomPlaybackTime)
+                                send(IPC.Request.SetProperty(IPC.Property.PLAYBACK_TIME, randomPlaybackTime))
                             }
-                            println("Received message: $message")
+                        }
+
+                        launch {
+                            while (true) {
+                                val requestedPlaybackTime = withContext(Dispatchers.IO) { objectInputStream.readInt() }
+                                send(IPC.Request.SetProperty(IPC.Property.PLAYBACK_TIME, requestedPlaybackTime))
+                            }
                         }
                     }
 
-                    launch {
-                        while (true) {
-                            val message = withContext(Dispatchers.IO) {
-                                readln()
-                            }
-                            objectOutputStream.writeUTF(message)
-                        }
-                    }
+                    MPV(requests, this)
                 }
             }
         }
--- Server/src/main/kotlin/Server.kt
+++ Server/src/main/kotlin/Server.kt
@@ -1,8 +1,5 @@
 package tfg.proto.shareplay
 
-import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.ReceiveChannel
-import kotlinx.coroutines.channels.produce
 import java.io.DataInputStream
 import java.io.DataOutputStream
 import java.net.InetAddress
@@ -10,73 +7,56 @@ import java.net.ServerSocket
 import java.net.Socket
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ConcurrentLinkedQueue
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 val rooms = Rooms()
 
-fun main() = runBlocking {
-    val roomClients = roomClientProducer(clientSocketProducer())
+suspend fun main() = coroutineScope {
+    val newClients = newClientsProducer()
 
-    for (roomClient in roomClients) {
+    newClients.consumeEach { newClient ->
         launch {
-            val roomId = roomClient.getRoomId()
-
-            rooms.addIfAbsent(roomId).apply {
-                handleNewClient(roomClient)
-            }
+            rooms.handleNewClient(newClient)
         }
     }
 }
 
 @OptIn(ExperimentalCoroutinesApi::class)
-fun CoroutineScope.clientSocketProducer() = produce {
+fun CoroutineScope.newClientsProducer() = produce {
     ServerSocket(1234, 50, InetAddress.getByName("localhost")).use { serverSocket ->
-        println("Server started.")
+        println("Server started at ${serverSocket.inetAddress.hostAddress}:${serverSocket.localPort}")
         while (!serverSocket.isClosed) {
-            println("Waiting for client connection...")
+            println("Waiting for new client...")
             val socket = withContext(Dispatchers.IO) {
                 serverSocket.accept()
             }
-            println("Client connected.")
-            send(socket)
+            println("New client connected: ${socket.inetAddress.hostAddress}:${socket.port}")
+            send(Room.Client(socket))
         }
+        println("Closing server socket...")
     }
-    println("Server stopped.")
+    println("Server stopped")
 }
 
-@OptIn(ExperimentalCoroutinesApi::class)
-fun CoroutineScope.roomClientProducer(clientsSockets: ReceiveChannel<Socket>) = produce {
-    for (clientSocket in clientsSockets) send(Room.Client(clientSocket))
-}
-
-class Room(private val id: Int) {
+class Room(private val name: String) {
     private val clients = ConcurrentLinkedQueue<Client>()
 
     suspend fun handleNewClient(client: Client) {
         clients.add(client)
 
-        println("Client $client added to room $id. Total clients: ${clients.size}")
-
-        //Notify the new client about the existing clients
-        clients.forEach {
-            if (it == client) return@forEach
-            client.sendMessage("Existing client: $it")
-        }
-
-        // Notify all clients in the room about the new client
-        clients.forEach {
-            if (it == client) return@forEach
-            it.sendMessage("New client joined: $client")
-        }
-
-        // Read messages from the client
         while (true) {
-            val message = client.readMessage()
-            println("Received message from $client: $message")
+            val number = client.readInt()
 
-            // Broadcast the message to all clients in the room
-            clients.forEach {
-                if (it == client) return@forEach
-                it.sendMessage("Message from $client: $message")
+            clients.filterNot { it == client }.forEach {
+                it.sendInt(number)
             }
         }
     }
@@ -87,28 +67,28 @@ class Room(private val id: Int) {
         private val dataInputStream: DataInputStream = DataInputStream(socket.inputStream)
         private val dataOutputStream: DataOutputStream = DataOutputStream(socket.outputStream)
 
-        suspend fun getRoomId(): Int {
-            return withContext(Dispatchers.IO) { dataInputStream.readInt() }
-        }
+        suspend fun readInt() = withContext(Dispatchers.IO) { dataInputStream.readInt() }
 
-        internal suspend fun readMessage(): String {
-            return withContext(Dispatchers.IO) { dataInputStream.readUTF() }
-        }
+        internal suspend fun readUTF() = withContext(Dispatchers.IO) { dataInputStream.readUTF() }
 
-        internal fun sendMessage(message: String) {
-            dataOutputStream.writeUTF(message)
-        }
+        internal fun sendInt(value: Int) = dataOutputStream.writeInt(value)
 
-        override fun toString(): String {
-            return "${socket.inetAddress.hostAddress}:${socket.port}"
-        }
+        internal fun sendUTF(message: String) = dataOutputStream.writeUTF(message)
+
+        override fun toString() = "${socket.inetAddress.hostAddress}:${socket.port}"
     }
 }
 
 class Rooms {
-    private val rooms = ConcurrentHashMap<Int, Room>()
+    private val rooms = ConcurrentHashMap<String, Room>()
 
-    fun addIfAbsent(id: Int): Room {
-        return rooms.computeIfAbsent(id) { Room(id) }
+    fun createRoomIfAbsent(name: String) = rooms.computeIfAbsent(name) { Room(name) }
+
+    suspend fun handleNewClient(client: Room.Client) {
+        val roomName = client.readUTF()
+
+        createRoomIfAbsent(roomName).apply {
+            handleNewClient(client)
+        }
     }
 }
