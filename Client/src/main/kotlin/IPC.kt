53237af8cfe9c960f3dbe10b7f135aef52c2309d
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2025-05-16 18:46:35 +0200

    WIP. UNFINISHED. ipc response and event classes.

--- Client/src/main/kotlin/IPC.kt
+++ Client/src/main/kotlin/IPC.kt
@@ -1,6 +1,7 @@
 package tfg.proto.shareplay
 
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.add
 import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.put
@@ -84,4 +85,111 @@ object IPC {
             parameters = listOf(filePath),
         )
     }
+
+    sealed class Response(
+        internal val error: String,
+        internal val data: Any? = null,
+        internal val requestId: Int? = null,
+    )
+
+    sealed class Event(
+        internal val type: Type,
+        internal open val id: Int? = null,
+        internal open val error: String? = null,
+    ) {
+
+        data class StartFile(
+            internal val playlistEntryId: Int,
+            override val id: Int? = null,
+            override val error: String? = null,
+        ) : Event(
+            type = Type.START_FILE,
+        )
+
+        data class EndFile(
+            internal val reason: Reason,
+            internal val playlistEntryId: Int,
+            internal val fileError: String? = null,
+            internal val playlistInsertId: Int? = null,
+            internal val playlistInsertNumEntries: Int? = null,
+            override val id: Int? = null,
+            override val error: String? = null,
+        ) : Event(
+            type = Type.END_FILE,
+        ) {
+            enum class Reason(val value: String) {
+                EOF("eof"),
+                STOP("stop"),
+                QUIT("quit"),
+                ERROR("error"),
+                REDIRECT("redirect"),
+                UNKNOWN("unknown"),
+            }
+        }
+
+        data class LogMessage(
+            internal val prefix: String,
+            internal val level: String,
+            internal val message: String,
+            override val id: Int? = null,
+            override val error: String? = null,
+        ) : Event(
+            type = Type.LOG_MESSAGE,
+        )
+
+        data class Hook(
+            internal val hookId: Int,
+            override val id: Int? = null,
+            override val error: String? = null,
+        ) : Event(
+            type = Type.HOOK,
+        )
+
+        data class CommandReply(
+            internal val result: Any,
+            override val id: Int? = null,
+            override val error: String? = null,
+        ) : Event(
+            type = Type.COMMAND_REPLY,
+        )
+
+        data class ClientMessage(
+            internal val args: List<String>,
+            override val id: Int? = null,
+            override val error: String? = null,
+        ) : Event(
+            type = Type.CLIENT_MESSAGE,
+        )
+
+        data class PropertyChange(
+            internal val property: Property,
+            internal val value: Any,
+            override val id: Int? = null,
+            override val error: String? = null,
+        ) : Event(
+            type = Type.PROPERTY_CHANGE,
+        )
+
+        enum class Type(val value: String) {
+            START_FILE("start-file"),
+            END_FILE("end-file"),
+            FILE_LOADED("file-loaded"),
+            SEEK("seek"),
+            PLAYBACK_RESTART("playback-restart"),
+            SHUTDOWN("shutdown"),
+            LOG_MESSAGE("log-message"),
+            HOOK("hook"),
+            GET_PROPERTY_REPLY("get-property-reply"),
+            SET_PROPERTY_REPLY("set-property-reply"),
+            COMMAND_REPLY("command-reply"),
+            CLIENT_MESSAGE("client-message"),
+            VIDEO_RECONFIG("video-reconfig"),
+            AUDIO_RECONFIG("audio-reconfig"),
+            PROPERTY_CHANGE("property-change"),
+            IDLE("idle"),
+            TICK("tick"),
+        }
+    }
+
+    fun receive(rawJson: String) = Json.decodeFromString<Response>(rawJson)
 }
