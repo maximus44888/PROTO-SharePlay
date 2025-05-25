3b53bdb9e1e149d001d77d26045878f1e4ecd5af
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2025-05-25 16:13:11 +0200

    moved IPC to the MPV class. Also changed how request execution works

--- Client/src/main/kotlin/MPV.kt
+++ Client/src/main/kotlin/MPV.kt
@@ -5,8 +5,6 @@ import java.io.File
 import java.io.PrintWriter
 import java.lang.Thread.sleep
 import java.net.Socket
-import java.util.concurrent.ConcurrentHashMap
-import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -15,20 +13,28 @@ import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.shareIn
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.add
 import kotlinx.serialization.json.booleanOrNull
+import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.doubleOrNull
 import kotlinx.serialization.json.intOrNull
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
+import kotlinx.serialization.json.put
+import kotlinx.serialization.json.putJsonArray
 import org.scalasbt.ipcsocket.Win32NamedPipeSocket
+import kotlin.concurrent.atomics.AtomicInt
+import kotlin.concurrent.atomics.ExperimentalAtomicApi
+import kotlin.concurrent.atomics.fetchAndIncrement
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
@@ -39,7 +45,6 @@ class MPV(
     private val writer: PrintWriter
     private val reader: BufferedReader
     private val responses: SharedFlow<JsonObject>
-    private val requests = ConcurrentHashMap<Int, CompletableDeferred<JsonObject>>()
 
     init {
         @OptIn(ExperimentalUuidApi::class)
@@ -68,23 +73,12 @@ class MPV(
                 emit(Json.parseToJsonElement(responseLine).jsonObject)
             }
         }.shareIn(CoroutineScope(Dispatchers.IO), SharingStarted.Eagerly, 0)
-
-        CoroutineScope(Dispatchers.IO).launch {
-            responses.collect {
-                val requestId = it["request_id"]?.jsonPrimitive?.intOrNull
-                if (requestId != null) {
-                    requests[requestId]?.complete(it)
-                    requests.remove(requestId)
-                }
-            }
-        }
     }
 
     private suspend fun IPC.Request.execute(): JsonObject {
-        val completableDeferred = CompletableDeferred<JsonObject>()
-        requests[requestId] = completableDeferred
-        writer.println(jsonString)
-        return completableDeferred.await()
+        return responses
+            .onStart { writer.println(jsonString) }
+            .first { it["request_id"]?.jsonPrimitive?.intOrNull == requestId }
     }
 
     override suspend fun loadFile(fileIdentifier: String) {
@@ -123,5 +117,99 @@ class MPV(
             .map { getPlaybackTime() }
             .filterNotNull()
             .shareIn(CoroutineScope(Dispatchers.IO), SharingStarted.Eagerly, 0)
+    }
+
+    object IPC {
+        internal enum class Command(val value: String) {
+            GET_PROPERTY("get_property"),
+            SET_PROPERTY("set_property"),
+            OBSERVE_PROPERTY("observe_property"),
+            LOAD_FILE("loadfile"),
+        }
+
+        enum class Property(val value: String) {
+            PAUSE("pause"),
+            PLAYBACK_TIME("playback-time"),
+        }
+
+        @OptIn(ExperimentalAtomicApi::class)
+        sealed class Request(
+            internal val command: Command,
+            internal val parameters: List<Any>,
+            internal open val async: Boolean? = null,
+        ) {
+            private companion object {
+                private val _requestId = AtomicInt(0)
+            }
+
+            val requestId = _requestId.fetchAndIncrement()
+
+            @OptIn(ExperimentalSerializationApi::class)
+            val jsonString = buildJsonObject {
+                putJsonArray("command") {
+                    add(command.value)
+                    parameters.forEach { param ->
+                        when (param) {
+                            is String -> add(param)
+                            is Number -> add(param)
+                            is Boolean -> add(param)
+                            is Property -> add(param.value)
+                        }
+                    }
+                }
+                put("request_id", requestId)
+                async?.let { put("async", it) }
+            }.toString()
+
+            data class GetProperty(
+                internal val property: Property,
+                override val async: Boolean? = null,
+            ) : Request(
+                command = Command.GET_PROPERTY,
+                parameters = listOf(property),
+            )
+
+            data class SetProperty(
+                internal val property: Property,
+                internal val value: Any,
+                override val async: Boolean? = null,
+            ) : Request(
+                command = Command.SET_PROPERTY,
+                parameters = listOf(property, value),
+            )
+
+            @ConsistentCopyVisibility
+            @OptIn(ExperimentalAtomicApi::class)
+            data class ObserveProperty private constructor(
+                val id: Int,
+                private val property: Property,
+                override val async: Boolean? = null,
+            ) : Request(
+                command = Command.OBSERVE_PROPERTY,
+                parameters = listOf(id, property),
+            ) {
+                constructor(property: Property, async: Boolean? = null) : this(
+                    id = _id.fetchAndIncrement(),
+                    property = property,
+                    async = async
+                )
+
+                private companion object {
+                    private val _id = AtomicInt(1)
+                }
+            }
+
+            data class LoadFile(
+                internal val filePath: String,
+                override val async: Boolean? = null,
+            ) : Request(
+                command = Command.LOAD_FILE,
+                parameters = listOf(filePath),
+            )
+        }
+
+        enum class EventType(val value: String) {
+            SEEK("seek"),
+        }
     }
 }
