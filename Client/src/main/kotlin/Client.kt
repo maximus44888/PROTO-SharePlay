6a3445ff797df93b9a391b6f7bc7e79d7419efd8
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2025-05-31 19:29:25 +0200

    trying PlayerClient fixes

--- Client/src/main/kotlin/Client.kt
+++ Client/src/main/kotlin/Client.kt
@@ -6,7 +6,6 @@ import java.net.Socket
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
 class PlayerClient(
@@ -15,37 +14,39 @@ class PlayerClient(
     private val player: Player,
 ) {
     init {
-        socket.withObjectStreams { output, input ->
-            output.writeUTF(roomName)
-            output.flush()
+        ObjectOutputStream(socket.getOutputStream()).use { output ->
+            ObjectInputStream(socket.getInputStream()).use { input ->
+                output.writeUTF(roomName)
+                output.flush()
 
-            coroutineScope {
-                launch(Dispatchers.IO) {
-                    player.loadedMediaEvents.collect {
-                        output.writeObject(NetworkEvent.MediaLoaded(it))
-                        output.flush()
+                CoroutineScope(Dispatchers.IO).launch {
+                    launch(Dispatchers.IO) {
+                        player.loadedMediaEvents.collect {
+                            output.writeObject(NetworkEvent.MediaLoaded(it))
+                            output.flush()
+                        }
                     }
-                }
-                launch(Dispatchers.IO) {
-                    player.pauseEvents.collect {
-                        output.writeObject(NetworkEvent.Pause(it))
-                        output.flush()
+                    launch(Dispatchers.IO) {
+                        player.pauseEvents.collect {
+                            output.writeObject(NetworkEvent.Pause(it))
+                            output.flush()
+                        }
                     }
-                }
-                launch(Dispatchers.IO) {
-                    player.seekEvents.collect {
-                        output.writeObject(NetworkEvent.Seek(it))
-                        output.flush()
+                    launch(Dispatchers.IO) {
+                        player.seekEvents.collect {
+                            output.writeObject(NetworkEvent.Seek(it))
+                            output.flush()
+                        }
                     }
-                }
 
-                launch(Dispatchers.IO) {
-                    while (true) {
-                        input.readObject().let { event ->
-                            when (event) {
-                                is NetworkEvent.MediaLoaded -> player.loadMedia(event.mediaURI)
-                                is NetworkEvent.Pause -> if (event.paused) player.pause() else player.resume()
-                                is NetworkEvent.Seek -> player.seek(event.time)
+                    launch(Dispatchers.IO) {
+                        while (true) {
+                            input.readObject().let { event ->
+                                when (event) {
+                                    is NetworkEvent.MediaLoaded -> player.loadMedia(event.mediaURI)
+                                    is NetworkEvent.Pause -> if (event.paused) player.pause() else player.resume()
+                                    is NetworkEvent.Seek -> player.seek(event.time)
+                                }
                             }
                         }
                     }
@@ -53,13 +54,4 @@ class PlayerClient(
             }
         }
     }
-
-    fun Socket.withObjectStreams(block: suspend (ObjectOutputStream, ObjectInputStream) -> Unit) {
-        ObjectOutputStream(getOutputStream()).use { output ->
-            ObjectInputStream(getInputStream()).use { input ->
-                CoroutineScope(Dispatchers.IO).launch { block(output, input) }
-            }
-        }
-    }
-
 }
