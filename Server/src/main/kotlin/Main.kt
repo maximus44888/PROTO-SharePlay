61ac0f49f514eaa19b4361354d07a677bd2f0eb5
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2025-06-04 13:30:10 +0200

    refactor: optimize coroutine scope usage in server socket handling

--- Server/src/main/kotlin/Main.kt
+++ Server/src/main/kotlin/Main.kt
@@ -5,6 +5,7 @@ import java.net.ServerSocket
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -12,12 +13,14 @@ suspend fun main(vararg args: String) {
     val bindAddress = args.getOrNull(0) ?: "0.0.0.0"
     val port = args.getOrNull(1)?.toIntOrNull() ?: 1234
 
+    val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
     ServerSocket(port, 50, InetAddress.getByName(bindAddress)).use { serverSocket ->
         println("Server started on $bindAddress:$port")
 
         while (!serverSocket.isClosed) {
             val socket = withContext(Dispatchers.IO) { serverSocket.accept() }
-            CoroutineScope(Dispatchers.IO).launch { Room.handleNewClient(Room.Client(socket)) }
+            scope.launch { Room.handleNewClient(Room.Client(socket)) }
         }
     }
 }
