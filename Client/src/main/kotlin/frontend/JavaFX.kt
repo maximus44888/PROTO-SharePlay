14a57bcefd83f3e026eaf5abdcb90a35394424ca
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2025-06-08 21:39:50 +0200

    feat: add application icon to JavaFX stage

--- Client/src/main/kotlin/frontend/JavaFX.kt
+++ Client/src/main/kotlin/frontend/JavaFX.kt
@@ -3,6 +3,7 @@ package tfg.proto.shareplay.frontend
 import java.net.URL
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
+import javafx.scene.image.Image
 import javafx.stage.Stage
 import javafx.util.Callback
 
@@ -15,6 +16,9 @@ object JavaFX {
             }
         }
 
-        return stage.apply { scene = Scene(loader.load()) }
+        return stage.apply {
+            icons.add(Image(javaClass.getResourceAsStream("/frontend/shareplay.png")))
+            scene = Scene(loader.load())
+        }
     }
 }
